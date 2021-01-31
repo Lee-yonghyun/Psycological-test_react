b545e3d423eadb1a89f2b61480693b0967d1ee92
이용현 <dydwk231@naver.com>
2021-01-31 12:08:09 +0900

    apply to history for store

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -1,10 +1,17 @@
-import { combineReducers, createStore as reduxCreateStore } from "redux";
+import {
+  combineReducers,
+  createStore as reduxCreateStore,
+  applyMiddleware,
+} from "redux";
+import { connectRouter, routerMiddleware } from "connected-react-router";
 import { UsersReducer } from "../users/reducers";
 
-export default function createStore() {
+export default function createStore(history) {
   return reduxCreateStore(
     combineReducers({
       users: UsersReducer,
-    })
+      router: connectRouter,
+    }),
+    applyMiddleware(routerMiddleware(history))
   );
 }
