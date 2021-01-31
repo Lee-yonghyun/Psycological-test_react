7d8648a061d914d1292a8050af11c3873557d41a
이용현 <dydwk231@naver.com>
2021-01-31 11:57:14 +0900

    create templates(components)

---
+++ src/templates/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from "./Home";
+export { default as One } from "./One";
+export { default as Second } from "./Second";
+export { default as Second } from "./Second";
+export { default as Result } from "./Third";
